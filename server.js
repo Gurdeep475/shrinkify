a57fee86966f14788feddc48015d2c6b8d0f8d29
Gurdeep Singh <gurdeepsingh475475@gmail.com>
2022-02-10 09:20:22 +0530

    logo and favicon added, body parser and path added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const { urlencoded } = require('express');
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
 app.set('view engine','ejs');
 app.set('views','views');
 app.listen(process.env.PORT ||3000);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res,next) => {
     res.render('index');
