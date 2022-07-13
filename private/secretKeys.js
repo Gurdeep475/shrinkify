63931cff2f0803b8249e11dcfb1dbf8e2a182646
Gurdeep Singh <32095032+Gurdeep475@users.noreply.github.com>
2022-07-13 11:38:43 +0530

    Update secretKeys.js

--- private/secretKeys.js
+++ private/secretKeys.js
@@ -1,3 +1,3 @@
-const key = "mongodb+srv://gurdeepsingh475475:453113@cluster0.jdu7q.mongodb.net/shrinkify?retryWrites=true&w=majority";
+const key = "mongodb+srv://gurdeepsingh475475:******@cluster0.jdu7q.mongodb.net/shrinkify?retryWrites=true&w=majority";
 
 module.exports = key;
